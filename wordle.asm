373481a08d8b309e936d6a87cd5f604a8c42dafd
Spencer Hirsch <spencer@yellowcrest.com>
2022-11-09 00:52:52 -0500

    Slow progress.

--- wordle.asm
+++ wordle.asm
@@ -3,14 +3,17 @@
 include Irvine32.inc
 
 .data
-word1 BYTE "hello",0
-word2 BYTE "start",0
-word3 BYTE "catch",0
-word4 BYTE "tacos",0
+list DWORD "hello", "start", "catch", "tacos",0
+user_input BYTE ?,0
+max BYTE 5,0
 
 .code
 main PROC
-
+mov ecx, 5
+loop L1:
+	; take input from the user
+	; needs to execute 5 times if the result was not found beforehan
+end L1
 
 endp
 END main
