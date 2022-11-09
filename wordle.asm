df63a3e2fb570af838b0f6f1c7c557b15df41260
Spencer Hirsch <spencer@yellowcrest.com>
2022-11-09 00:34:33 -0500

    Beginning the project.

---
+++ wordle.asm
@@ -0,0 +1,17 @@
+.386
+.model flat, stdcalll
+include Irvine32.inc
+
+.data
+word1 BYTE "hello",0
+word2 BYTE "start",0
+word3 BYTE "catch",0
+word4 BYTE "tacos",0
+
+.code
+main PROC
+
+
+endp
+END main
+
