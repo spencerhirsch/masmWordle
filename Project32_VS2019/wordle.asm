7201bc33760ba4bc455f96f1ba440021fefe6479
spencerhirsch <spencer@yellowcrest.com>
2022-11-10 12:45:58 -0800

    Take message from user in beginning for the comparison.

--- Project32_VS2019/wordle.asm
+++ Project32_VS2019/wordle.asm
@@ -3,7 +3,6 @@
 INCLUDE Irvine32.inc
 
 .data
-;list DWORD "hello", "start", "catch", "tacos",0
 
 ; Messages that are displayed in the console once the program begins
 intro BYTE "Welcome to WORDLE!",0
@@ -12,23 +11,38 @@ rule1 BYTE "1. The inputted word must be 5 characters.",0
 rule2 BYTE "2. All words can be found in the English dictionary.",0
 rule3 BYTE "3. You only have 6 chances to figure out the word.",0
 luck BYTE "Good Luck!",0
+; Messages displayed as the user inputs words into the console
 input_string BYTE "Input: ",0
 attempt_string BYTE "Attempt: ",0
 
+; Variable that is used to store the user input from the ReadString function
 user_input BYTE 5 DUP(?)		; Limit on the number of characters that
-								; can be read from the user
+								; can be read from the user by setting size
+								; to 5
+
+; String given by the user when the program begins, this is the string that
+; will be used for comparisons from the user input
+true_string BYTE 5 DUP(?)
+prompt_message BYTE "Input expected String: ",0
+
+index BYTE 1
 
 .code
 main PROC
- CALL OutputLoad
+ call OutputLoad
+ call CollectString
+ call WaitMsg
+ call Crlf
+ call Crlf
 ; call SetTextColor ( will use to show incorrect letters, correct placement, and valid letter but incorrect placement. )
 ; mov eax, green
 ; call SetTextColor
 ; revert color back to white, do this for each character read in from the user.
  
+ mov al, index
  mov edi,6
  L1:
-  CALL ProcessInput		; Want to implement a check to see if the user inputted a value that is actually of length 5
+  call ProcessInput		; Want to implement a check to see if the user inputted a value that is actually of length 5
 							; If greater than 5 output to the console and and notify the user and allow for reinput
 
 							; implement a state machine to do character comparison, output each character in a color
@@ -37,7 +51,8 @@ main PROC
 
 							; would like to have a display at the bottom of the screen of all valid characters, after each
 							; input the list of characters would be updated to show whether that was tried and not in the word 
-  
+  inc al
+  mov index, al
   push edi
   pop edi
   dec edi
@@ -83,11 +98,27 @@ OutputLoad PROC
  call WriteString
  call Crlf
  call Crlf
- call WaitMsg
  ; After the user has moved on clear the screen.
- call ClrScr
  ret
 OutputLoad ENDP
+
+
+;======================================================
+;				    CollectString PROC
+; Procedure that takes input for the value of the string
+; that the comparisons will be paried against.
+;======================================================
+CollectString PROC
+ mov edx, OFFSET prompt_message
+ call WriteString
+ ;call DisableEcho ; Need to implement
+ mov edx, OFFSET true_string
+ mov ecx, (LENGTHOF true_string) + 1
+ call ReadString
+ ;call EnableEcho : Need to implement
+ call Crlf
+ ret
+CollectString ENDP
 
 ;======================================================
 ;					ProcessInput PROC
@@ -104,6 +135,9 @@ ProcessInput PROC
 ; implement a state machine to do the comparison for all values of the input.
  mov edx, OFFSET attempt_string
  call WriteString
+ ;mov index, al
+ ;mov edx, OFFSET index
+ ;call WriteInt
  mov edx, OFFSET user_input
  call WriteString
  call Crlf
