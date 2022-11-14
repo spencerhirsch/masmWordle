5b6ab41d4c1284b240a982e64e3ecc00485a4362
spencerhirsch <spencer@yellowcrest.com>
2022-11-13 20:23:20 -0800

    adding new features.

--- Project32_VS2019/wordle.asm
+++ Project32_VS2019/wordle.asm
@@ -1,42 +1,74 @@
+;--------------------------------------------------------
+; Authors: Spencer Hirsch, shirsch2020@my.fit.edu, James
+;          Pabisz, jpabisz2020@my.fit.edu
+; Program: Wordle in Assembly (Contest 1)
+; Course: Computer Architecture and Assembly, Fall 2022
+;--------------------------------------------------------
+
 .386
 .model flat, stdcall
 INCLUDE Irvine32.inc
 
 .data
-
-; Messages that are displayed in the console once the program begins
+;--------------------------------------------------------
+; All of the messages displayed to the user upon
+; beginning the program.
+;--------------------------------------------------------
 intro BYTE "Welcome to WORDLE!",0
 ruleIntro BYTE "The rules of the game are simple:",0
-rule1 BYTE "1. The inputted word must be 5 characters.",0
-rule2 BYTE "2. All words can be found in the English dictionary.",0
-rule3 BYTE "3. You only have 6 chances to figure out the word.",0
+rule1 BYTE "1. This is more fun with two people, therefore, a two player game.",0
+rule2 BYTE "2. One user must input a word for the other to guess.",0
+rule3 BYTE "3. The inputted word must be 5 characters.",0
+rule4 BYTE "4. You only have 6 chances to figure out the word.",0
+rule5 BYTE "5. There are a possible 600 points to score.",0
+
+;--------------------------------------------------------
+; All of the potential messages that are used upon the 
+; completion of the program.
+;--------------------------------------------------------
 luck BYTE "Good Luck!",0
 fail BYTE "Better Luck Next Time!",0
 pass BYTE "Wow! That Was Impressive!",0
+ok_message BYTE "That was ok, you can do better.",0
 score_message BYTE "You scored: ",0
 points_message BYTE " Points",0
-points DWORD 600
+minus_points BYTE "-100 Points!",0
+current_points BYTE "Potential points: ",0
 
-; Messages displayed as the user inputs words into the console
+points DWORD 600      ; Initial number of potential points
+
+;--------------------------------------------------------
+; Messages displayed to the use once the program begins
+; taking input during gameplay.
+;--------------------------------------------------------
 input_string BYTE "Input: ",0
 attempt_string BYTE "Attempt: ",0
 
-; Variable that is used to store the user input from the ReadString function
-user_input BYTE 6 DUP(?)		; Limit on the number of characters that
-								; can be read from the user by setting size
-								; to 5
+;--------------------------------------------------------
+; Variable that is used to store the user input from the 
+; ReadString function. Limit on the number of characters 
+; that can be read from the user by setting size to 5 plus 
+; the null pointer.
+;--------------------------------------------------------
+user_input BYTE 6 DUP(?)
 
-; String given by the user when the program begins, this is the string that
-; will be used for comparisons from the user input
+;--------------------------------------------------------
+; String given by the user when the program begins, this 
+; is the string that will be used for comparisons from the 
+; user input
+;--------------------------------------------------------
 true_string BYTE 6 DUP(?)
 prompt_message BYTE "Input expected String: ",0
 
-temp_true BYTE ?
-temp_user BYTE ?
-
-index BYTE 1
+index BYTE 1        ; Used for number of attempts
 
 .code
+;--------------------------------------------------------
+; Used as a driver for the program. Call all of the
+; necessary procedures for the program to execute 
+; properly. Calculates the number of attempts as well as
+; the number of points a user can receive.
+;--------------------------------------------------------
 main PROC
  ; Call the procedures
  call OutputLoad
@@ -58,6 +90,15 @@ main PROC
   mov eax, points
   sub eax, 100
   mov points, eax
+  ;call Crlf
+  mov edx, OFFSET current_points
+  call WriteString
+  mov eax, points
+  call WriteInt
+  mov edx, OFFSET points_message
+  call WriteString
+  call Crlf
+  call Crlf
   pop eax		
   inc al
   mov index, al
@@ -79,12 +120,12 @@ call Crlf
 INVOKE ExitProcess,0
 main ENDP
 
-;======================================================
-;					OutoutLoad PROC
+;--------------------------------------------------------
+;					OutputLoad PROC
 ; Function outputs the rules of the game to the console
 ; before waiting for user input. Allows the user to 
 ; understand how to play the game.
-;======================================================
+;--------------------------------------------------------
 
 OutputLoad PROC
  ; Write out the introductory message when the program is run.
@@ -107,10 +148,16 @@ OutputLoad PROC
  call Crlf
  mov edx, OFFSET rule3		; Write the last rule to the console
  call WriteString
+ mov edx, OFFSET rule4		; Write the first rule to the console
+ call WriteString
+ call Crlf
+ mov edx, OFFSET rule5		; Write the second rule to the console
+ call WriteString
+ call Crlf
  call Crlf
  call Crlf
  mov dl,29
- mov dh,8
+ mov dh,10
  call GoToXY
  mov edx, OFFSET luck		; Output final message before program begins
  call WriteString
@@ -172,17 +219,32 @@ ProcessInput PROC
   call SetTextColor
   call Crlf
   call Crlf
-  mov edx, OFFSET pass
-  call WriteString
-  call Crlf
-  mov edx, OFFSET score_message
-  call WriteString
+
   mov eax, points
-  call WriteInt
-  mov edx, OFFSET points_message
-  call WriteString
-  call Crlf
-  INVOKE ExitProcess,0            ; Input was correct, terminate the program
+  cmp eax, 300
+  jae Good
+  jb Ok
+  
+  Good:
+   mov edx, OFFSET pass
+   call WriteString
+   jmp Break
+  
+  Ok:
+   mov edx, OFFSET ok_message
+   call WriteString
+   jmp Break
+  
+  Break:
+   call Crlf
+   mov edx, OFFSET score_message
+   call WriteString
+   mov eax, points
+   call WriteInt
+   mov edx, OFFSET points_message
+   call WriteString
+   call Crlf
+   INVOKE ExitProcess,0            ; Input was correct, terminate the program
 
  NotEqual:                        ; If complete equality was not acheived
  mov edi, OFFSET [user_input]       ; Reinitialize registers
@@ -254,6 +316,9 @@ ProcessInput PROC
     jne outer
 
  call Crlf
+ call Crlf
+ mov edx, OFFSET minus_points
+ call WriteString
  call Crlf
  ret
 ProcessInput ENDP
